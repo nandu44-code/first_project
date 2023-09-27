0dc82f3a997ab508353afdb52395b9fcb128379e
nandu44-code <nanduganeshnair4444@gmail.com>
2023-09-27 18:22:13 +0530

    remove item from cart increment and decrement item from cart

--- furnics/carts/urls.py
+++ furnics/carts/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     
       
-      
-
+       path('',views.cart_page,name='cart_page'),
+       path('add-cart/<int:product_id>/',views.add_cart,name='add_cart'),
+       path('remove-cart/<int:product_id>/',views.decrement_cartitem,name="decrement_cartitem"),
 ]
