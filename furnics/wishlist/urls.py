7e64b18a3157317379dcfd85effeb8579d224c00
nandu44-code <nanduganeshnair4444@gmail.com>
2023-11-10 11:48:59 +0530

    fear(add to wishlist):added

--- furnics/wishlist/urls.py
+++ furnics/wishlist/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.wishlist,name="wishlist_view"),
     path('add-to-wishlist<int:product_id>',views.add_to_wishlist,name="add_to_wishlist"),
+    
 ]
--- furnics/wishlist/views.py
+++ furnics/wishlist/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from accounts.models import CustomUser
+from store.models import Variation
 from .models import WishlistItem
+from django.contrib import messages
+
 # Create your views here.
 def wishlist(request):
 
@@ -15,5 +20,28 @@ def wishlist(request):
     return render(request,'userprofile/wishlist.html',context)
 def add_to_wishlist(request,product_id):
 
+    variant = Variation.objects.get(id=product_id)
+    print(variant.id)
+    user=request.user
+    print(user.email)
+    try:
+        is_exist = WishlistItem.objects.filter(user = user, product_name=variant).exists()
 
-    
+        if is_exist:
+            # Wishlist item already exists
+            messages.error(request, 'This product is already in your wishlist.')
+        else:
+            # Wishlist item does not exist, add it
+            wishlist = WishlistItem(user=user, product_name=variant)
+            wishlist.save()
+            messages.success(request, 'Product added to wishlist.')
+
+        return redirect('wishlist_view')
+
+    except Exception as e:
+        # Handle exceptions if any
+        # Log or handle the exception accordingly
+        print(e)
+        messages.error(request, 'Failed to add the product to the wishlist.')
+        return redirect('wishlist_view')
+
